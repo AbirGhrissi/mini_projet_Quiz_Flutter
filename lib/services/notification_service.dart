52c76d02da51f9f4b5dcc17c70a3da22dccf7579
AbirGhrissi <abirghrissi83@gmail.com>
2025-05-17 11:30:15 +0100

    Refactor NotificationService to use flutter_local_notifications instead of awesome_notifications

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,36 +1,46 @@
-import 'dart:ui';
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
   static final NotificationService _instance = NotificationService._internal();
   factory NotificationService() => _instance;
   NotificationService._internal();
 
-  void initialize() {
-    AwesomeNotifications().initialize(
-      'resource://drawable/res_app_icon',
-      [
-        NotificationChannel(
-          channelKey: 'basic_channel',
-          channelName: 'Basic notifications',
-          channelDescription: 'Notification channel for basic notifications',
-          defaultColor: Color(0xFF9D50DD),
-          ledColor: Colors.white,
-        ),
-      ],
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  void initialize() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
     );
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
   Future<void> showDailyReminder() async {
-    AwesomeNotifications().createNotification(
-      content: NotificationContent(
-        id: 10,
-        channelKey: 'basic_channel',
-        title: 'Quiz Time!',
-        body: 'Testez vos connaissances avec un nouveau quiz aujourd\'hui!',
-        notificationLayout: NotificationLayout.Default,
-      ),
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+      'basic_channel', // channel ID
+      'Basic notifications', // channel name
+      channelDescription: 'Notification channel for basic notifications',
+      importance: Importance.max,
+      priority: Priority.high,
+      color: Color(0xFF9D50DD),
+      enableLights: true,
+      ledColor: Colors.white,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      10,
+      'Quiz Time!',
+      'Testez vos connaissances avec un nouveau quiz aujourd\'hui!',
+      platformChannelSpecifics,
     );
   }
 }
